5a14647ac87fa473e1a2e9f0ccf66ded018f7e88
Nam Doan <nam.quang.doan@gmail.com>
2018-06-18 17:07:54 -0400

    Changing background colours

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 SECRETNUM = rand(101)
-guess = -1
+background = "grey"
 get '/' do
 	guess = params["guess"].to_i
 	message = check_guess(guess)
-	erb :index, :locals => {:number => SECRETNUM , :message => message}
+	background = backgroundcolor (message)
+	erb :index, :locals => {:number => SECRETNUM , :message => message, :guessColor => background}
 end
 
 def check_guess (guess)
-	if guess == -1
-		message = "Begin Guessing!"
+	if guess == 0
+		message = "Begin Guessing!";
 	elsif guess > SECRETNUM+5
-		message = "Way Too High!"
+		message = "Way Too High!";
 	elsif guess > SECRETNUM
 		message = "Too High"
 	elsif guess < SECRETNUM-5
 		message = "Way Too Low!"
 	elsif guess < SECRETNUM
 		message = "Too Low"
-	else
-		message = "Correct!"
+	else 
+		message = "Correct!";
 	end
+	return message
+end
+
+def backgroundcolor (message)
+	case message
+		when "Correct!"
+			background = "green"
+		when "Too Low"
+			background = "pink"
+		when "Too High"
+			background = "pink"
+		when "Way Too Low!"
+			background = "red"
+		when "Way Too High!"
+			background = "red"
+		else
+			background = "black"
+	end
+	return background
 end
