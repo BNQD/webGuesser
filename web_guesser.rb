62fe88560ce6d788234264bf3c7f9dfffe2abfdb
Nam Doan <nam.quang.doan@gmail.com>
2018-06-19 15:49:26 -0400

    Added Guesses counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,24 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
-SECRETNUM = rand(101)
+
+@@secretNum = rand(101)
 background = "grey"
+guessesRemaining = 5
+
 get '/' do
 	guess = params["guess"].to_i
 	message = check_guess(guess)
 	background = backgroundcolor (message)
-	erb :index, :locals => {:number => SECRETNUM , :message => message, :guessColor => background}
+	guessesRemaining, secretNum = decreaseGuesses(guessesRemaining)
+	erb :index, :locals => {
+		:number => secretNum , :message => message, :guessColor => background,
+		:numGuesses => guessesRemaining
+	}
 end
 
 def check_guess (guess)
 	if guess == 0
 		message = "Begin Guessing!";
-	elsif guess > SECRETNUM+5
+	elsif guess > @@secretNum+5
 		message = "Way Too High!";
-	elsif guess > SECRETNUM
+	elsif guess > @@secretNum
 		message = "Too High"
-	elsif guess < SECRETNUM-5
+	elsif guess < @@secretNum-5
 		message = "Way Too Low!"
-	elsif guess < SECRETNUM
+	elsif guess < @@secretNum
 		message = "Too Low"
 	else 
 		message = "Correct!";
@@ -43,3 +50,15 @@ def backgroundcolor (message)
 	end
 	return background
 end
+
+def decreaseGuesses (num)
+	if (num == 1)
+		randomNum = rand(101)
+		num = 5
+	else
+		num -= 1
+		randomNum = @@secretNum
+	end
+	return num, randomNum
+end
+
